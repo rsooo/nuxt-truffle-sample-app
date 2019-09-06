53dcc811dc122c7623f815bb98e2d09e661d7c48
akira saito <a-saito@bpm-gr.co.jp>
2019-09-06 22:35:05 +0900

    add ERC721token sample contract

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ const Contract = artifacts.require("ZombieFactory");
 const DemoContract = artifacts.require("DemoContract");
 const DemoToken = artifacts.require("DemoToken");
 const GLDToken = artifacts.require("GLDToken");
+const GameItem = artifacts.require("GameItem");
 
 module.exports = (deployer) => {
   deployer.deploy(Contract);
   deployer.deploy(DemoContract);
   deployer.deploy(DemoToken);
   deployer.deploy(GLDToken, 1);
+  deployer.deploy(GameItem);
 };
